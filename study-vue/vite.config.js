9a56700a0d055db85ce3f380dfb892dd2d41488f
jun <1132887706@qq.com>
2023-05-03 18:18:35 +0800

    完成首页

--- study-vue/vite.config.js
+++ study-vue/vite.config.js
@@ -1,7 +1,26 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from "path"
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  // ...
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    // 配置别名
+    alias: {
+      "@": resolve(__dirname, "src"),
+    }
+  }
 })
